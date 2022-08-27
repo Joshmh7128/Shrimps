1fa0287508c72ee486c0cb5ddc5c82a071a3f2fe
Josh <joshmh7128@gmail.com>
2022-08-27 13:45:54 -0400

    my money dont jiggle jiggle it wiggles im a shrimp bb

---
+++ Assets/Scripts/ShrimpWigglerScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrimpWigglerScript : MonoBehaviour
+{
+    float wiggleTime = 0.25f;
+    [SerializeField] float wiggleForce; // how hard do we wiggle
+
+    // all our wiggleable arms
+    [SerializeField] List<Rigidbody> wiggleArms = new List<Rigidbody>();
+
+    private void Start()
+    {
+        // start the wigglestep
+        StartCoroutine(WiggleStep());       
+    }
+
+    IEnumerator WiggleStep()
+    {
+        Debug.Log("running wiggle step");
+        // run our wiggle step
+        yield return new WaitForSecondsRealtime(wiggleTime);
+        // run our WiggleUpdate()
+        WiggleUpdate();
+        // restart 
+        StartCoroutine(WiggleStep());
+    }
+
+    // updates the wiggle
+    void WiggleUpdate()
+    {
+        Debug.Log("running wiggle update");
+        // wiggle the arms!
+        foreach (Rigidbody arm in wiggleArms)
+        {
+            Vector3 rand = new Vector3(Random.Range(-wiggleForce, wiggleForce), Random.Range(-wiggleForce, wiggleForce), Random.Range(-wiggleForce, wiggleForce));
+            arm.AddForce(rand, ForceMode.Impulse);
+        }
+    }
+}
